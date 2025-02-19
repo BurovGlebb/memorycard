3adb4bd78bf8c880ccfcec5488fb1bd8595fd9b8
Viktoria <167349092+BurovGlebb@users.noreply.github.com>
2025-02-19 18:30:38 +0300

    npc script

---
+++ NPC1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    public Vector3 newPosition;
+    
+    //Здоровье NPC
+    public int health = 5;
+
+    //Уровень NPC
+    public int level = 1;
+
+    //Скорость NPC
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
